9429efac3f288df9814df4462e8d10a34f215272
Harsh Rohila <rohilaharsh@gmail.com>
2022-07-01 08:46:12 +0530

    initial commit

---
+++ num_palindrome.cpp
@@ -0,0 +1,25 @@
+typedef unsigned int uint;
+
+class Solution {
+    
+uint rev(uint num) {
+    uint r = 0;
+    
+    while(num) {
+        uint ld = num % 10;
+        num /= 10;
+        
+        r = r*10 + ld;
+    }
+   
+    
+    return r;
+}
+    
+public:
+    bool isPalindrome(int x) {
+        if  (x < 0) return false;
+        
+        return x == rev(x);
+    }
+};
